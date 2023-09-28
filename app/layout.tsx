46704bcf08821cdbea82ee592ea2ba434c73987b
Sandesh Mankar <mankarsandesh11@gmail.com>
2023-09-28 14:55:08 +0700

    feat : update icon and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,26 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	variable: '--font-inter',
+})
+
+const spaceGrotesk = Space_Grotesk({
+	subsets: ['latin'],
+	weight: ['300', '400', '500', '600', '700'],
+	variable: '--font-spaceGrotesk',
+})
 
 export const metadata: Metadata = {
-	title: 'Sunday Dev ',
+	title: 'Sunday Dev',
 	description: 'Where all users can give answer to questions',
+	icons: {
+		icon: '/assets/images/site-logo.svg',
+	},
 }
 
 export default function RootLayout({
@@ -16,10 +30,18 @@ export default function RootLayout({
 }) {
 	return (
 		<ClerkProvider
-			publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+			appearance={{
+				elements: {
+					formButtonPrimary: 'primary-gradient',
+					footerActionLink: 'primary-text-gradient hover:text-primary-500',
+				},
+			}}
 		>
 			<html lang="en">
-				<body className={inter.className}>{children}</body>
+				<body className={` ${inter.variable} ${spaceGrotesk.varible} `}>
+					<h1 className="h1-bold">Hello Sandesh</h1>
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	)
